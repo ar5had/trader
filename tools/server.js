081b499507d50748be3bcb8c65ff08dbc36a6b2d
arshdkhn1 <arshdkhn1@gmail.com>
2017-02-16 23:58:54 +0530

    adding update

--- tools/server.js
+++ tools/server.js
@@ -8,27 +8,52 @@ import devConfig from '../webpack.config.dev';
 import prodConfig from '../webpack.config.prod';
 import express from 'express';
 import http from 'http';
-const mongoose = require('mongoose');
-const passport = require('passport');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const path = require('path');
+import mongoose from 'mongoose';
+import passport from 'passport';
+import session from 'express-session';
+import bodyParser from 'body-parser';
+import path from 'path';
 
-let config;
 const app = express();
 const server = http.createServer(app);
 
 /* eslint-disable no-console */
 console.log(chalkSuccess('Starting Express server...'));
+console.log(process.argv[2]);
 
 if (process.argv[2] !== "production") {
+
   require('dotenv').load({path: path.resolve(process.cwd() ,".env")});
-  config = devConfig;
+
+  const config = devConfig;
+  const bundler = webpack(config);
+  app.use(express.static('src/*.html'));
+  app.use(historyApiFallback());
+  app.use(webpackHotMiddleware(bundler));
+  app.use(webpackDevMiddleware(bundler, {
+    // Dev middleware can't access config, so we provide publicPath
+    publicPath: config.output.publicPath,
+
+    // These settings suppress noisy webpack output so only errors are displayed to the console.
+    noInfo: false,
+    quiet: false,
+    stats: {
+      assets: false,
+      colors: true,
+      version: false,
+      hash: false,
+      timings: false,
+      chunks: false,
+      chunkModules: false
+    },
+
+    // for other settings see
+    // http://webpack.github.io/docs/webpack-dev-middleware.html
+  }));
 } else {
-  config = prodConfig;
+
 }
 
-const bundler = webpack(config);
 
 require('../config/passport')(passport);
 
@@ -42,30 +67,6 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static('src/*.html'));
-app.use(historyApiFallback());
-app.use(webpackHotMiddleware(bundler));
-app.use(webpackDevMiddleware(bundler, {
-  // Dev middleware can't access config, so we provide publicPath
-  publicPath: config.output.publicPath,
-
-  // These settings suppress noisy webpack output so only errors are displayed to the console.
-  noInfo: false,
-  quiet: false,
-  stats: {
-    assets: false,
-    colors: true,
-    version: false,
-    hash: false,
-    timings: false,
-    chunks: false,
-    chunkModules: false
-  },
-
-  // for other settings see
-  // http://webpack.github.io/docs/webpack-dev-middleware.html
-}));
-server.listen(process.env.PORT);
 
 const options = {
   server: {
@@ -92,5 +93,6 @@ conn.on('error', console.error.bind(console, 'connection error:'));
 conn.once('open', () => {
   // Routes
 });
+server.listen(process.env.PORT);
 /* eslint-disable no-console */
 console.log(chalkSuccess('Express server is listening on port: ' + server.address().port));
