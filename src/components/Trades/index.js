b39d8888d1ab00c4851822451b6e18463dcb49eb
arshdkhn1 <arshdkhn1@gmail.com>
2017-02-24 17:37:39 +0530

    added trades page

--- src/components/Trades/index.js
+++ src/components/Trades/index.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import TradeRequest from '../TradeRequest/index';
+import ProposedTrade from '../ProposedTrade/index';
 import './styles.sass';
 
 class Trades extends Component {
   componentDidMount() {
     document.querySelector('.menu').classList.remove('open');
   }
+
+  getAllProposedTrades() {
+    return([
+      <ProposedTrade key="1"/>,
+      <ProposedTrade key="2"/>
+    ]);
+  }
+
+  getAllTradeRequests() {
+    return([
+      <TradeRequest key="1"/>,
+      <TradeRequest key="2"/>
+    ]);
+  }
+
   render() {
     return (
       <div className="tradesWrapper">
@@ -14,9 +31,15 @@ class Trades extends Component {
         <div className="tradesInfoWrapper">
           <div className="tradeReqWrapper">
             <h3 className="unCap">Trade Requests</h3>
+            <div className="allTradeRequestsWrapper">
+              {this.getAllTradeRequests()}
+            </div>
           </div>
           <div className="tradeProposedWrapper">
             <h3 className="unCap">Trade Proposed</h3>
+            <div className="allProposedTradesWrapper">
+              {this.getAllProposedTrades()}
+            </div>
           </div>
         </div>
       </div>
