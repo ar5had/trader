9b97da06432b1a8edd4095443ff8ffafada6ec35
Arshad Khan <arshdkhn1@gmail.com>
2018-06-03 19:07:26 +0530

    added some changes

--- config/auth.js
+++ config/auth.js
@@ -1,19 +1,22 @@
 'use strict';
 
+const APP_URL = 
+  process.env.NODE_ENV === "production" ? process.env.APP_URL : process.env.DEV_APP_URL;
+
 module.exports = {
   'facebookAuth': {
     'clientID': process.env.FACEBOOK_KEY,
     'clientSecret': process.env.FACEBOOK_SECRET,
-    'callbackURL': process.env.APP_URL + 'auth/facebook/callback'
+    'callbackURL': APP_URL + 'auth/facebook/callback'
   },
   'twitterAuth': {
     'clientID': process.env.TWITTER_KEY,
     'clientSecret': process.env.TWITTER_SECRET,
-    'callbackURL': process.env.APP_URL + 'auth/twitter/callback'
+    'callbackURL': APP_URL + 'auth/twitter/callback'
   },
   'googleAuth': {
     'clientID': process.env.GOOGLE_KEY,
     'clientSecret': process.env.GOOGLE_SECRET,
-    'callbackURL': process.env.APP_URL + 'auth/google/callback'
+    'callbackURL': APP_URL + 'auth/google/callback'
   }
 };
